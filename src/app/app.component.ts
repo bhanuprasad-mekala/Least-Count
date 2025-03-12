7755a01d55feb409be758b50c0e92f7bba977475
bhanuprasad-mekala <bhanuprasad.mekala@outlook.com>
2025-03-12 11:45:51 +0530

    change in background image URLs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LeastCountGame';
+  title = 'Least Count';
   
   isDivVisible = true;
 
